1cc3e56d31b2f48ede06e090413f4e75718073e0
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-08 19:53:44 +0300

    4.1.2. Глупый бот [#31323 #213538] Отработка замечаний (2 итерация) - в имя методов для тестов добавлено Then. Убраны лишние переводы строк

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class DummyBot {
 
     public static String answer(String question) {
-
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
@@ -14,14 +13,12 @@ public class DummyBot {
     }
 
     public static void main(String[] args) {
-
         String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
         rsl = DummyBot.answer("Как пройти в театры?");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
         System.out.println(rsl);
-
     }
 
 }
--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -8,33 +8,26 @@ import static org.junit.Assert.*;
 public class DummyBotTest {
 
     @Test
-    public void whenHy() {
-
+    public void whenHyTheyHy() {
         String in = "Привет, Бот.";
         String expected = "Привет, умник.";
         String out = DummyBot.answer(in);
         Assert.assertEquals(expected, out);
-
     }
 
     @Test
-    public void whenBye() {
-
+    public void whenByeThenBye() {
         String in = "Пока.";
         String expected = "До скорой встречи.";
         String out = DummyBot.answer(in);
         Assert.assertEquals(expected, out);
-
     }
 
     @Test
-    public void whenOther() {
-
+    public void whenOtherThenProblem() {
         String in = "Как пройти в театры.";
         String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
         String out = DummyBot.answer(in);
         Assert.assertEquals(expected, out);
-
     }
-
 }
