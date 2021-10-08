daf64d8720e56de37c2b21caa3f06e5ff5cb6758
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-08 21:41:54 +0300

    для задачи по составлению блок-схемы сравнения трех чисел

---
+++ src/main/java/other/WhereBig.java
@@ -0,0 +1,32 @@
+package other;
+
+public class WhereBig {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        // варианты 123, 231, 312, 132, 321, 213
+
+        a = 1;
+        b = 1;
+        c = 3;
+
+        if (a > b) {
+            if (a > c)
+                System.out.println("a");
+            else {
+                if (b > c)
+                    System.out.println("a");
+                else
+                    System.out.println("c");
+            }
+        }
+        else {
+            if (b > c)
+                System.out.println("b");
+            else
+                System.out.println("c");
+        }
+
+    }
+
+}
