503db465260f120f2ffe097ffad56e87275a581e
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-13 10:23:20 +0300

    5.7. Ипотека [#156315 #215264] Отработка замечаний

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,16 +4,11 @@ public class Mortgage {
 
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-
         while (amount > 0) {
-
             amount *= (1 + percent / 100);
             amount -= salary;
             year++;
-
         }
-
         return year;
     }
-
 }
