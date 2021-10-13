69e99845d11122af8c8d8f15bfb30c571d918f24
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-13 23:17:05 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenFind777ThenMinus1() {
+        int[] data = new int[] {1, 5, 6,8,23};
+        int el = 777;
+        int expected = -1;
+        int actual = FindLoop.indexOf(data, el);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenFind6Then3() {
+        int[] data = new int[] {1, 5, 6,8,23};
+        int el = 6;
+        int expected = 2;
+        int actual = FindLoop.indexOf(data, el);
+        Assert.assertEquals(expected, actual);
+    }
+}
