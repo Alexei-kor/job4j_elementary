0851e485a5bb3671a3c052595a385b2b5248b018
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-15 14:16:40 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #216242] Отработка замечаний - переделан цикл на for each

--- src/main/java/ru/job4j/array/Mashine.java
+++ src/main/java/ru/job4j/array/Mashine.java
@@ -8,11 +8,11 @@ public class Mashine {
         int[] rsl = new int[100];
         int size = 0;
         int change = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (change - coins[i] >= 0) {
-                rsl[size] = coins[i];
+        for (int coin: coins) {
+            while (change - coin >= 0) {
+                rsl[size] = coin;
                 size++;
-                change -= coins[i];
+                change -= coin;
             }
         }
         return Arrays.copyOf(rsl, size);
